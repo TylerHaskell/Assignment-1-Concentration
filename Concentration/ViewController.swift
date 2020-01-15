f7e2302dba633461a41b19f85bd166e2dae2ae78
Tyler Haskell <tyler.haskell@concur.com>
2020-01-15 15:05:23 -0800

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by I847323 on 1/15/20.
+//  Copyright © 2020 Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
